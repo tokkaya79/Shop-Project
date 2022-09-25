ccf66dbab8cb19d84f06bfb164efc8115c0b68eb
tokkaya79 <tokkaya79@gmail.com>
2022-09-25 16:08:45 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,14 @@ function Footer () {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+      <a 
+      className="grey-text text-lighten-4 right" 
+      href="https://github.com/tokkaya79/Shop-Project" 
+      target="_blank" 
+      rel="noreferrer"
+      >
+        Repo
+    </a>
       </div>
     </div>
   </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
 
     return   <nav className="deep-purple lighten-3">
                 <div className="nav-wrapper">
-                <a href="#!" className="brand-logo">React Shop</a>
+                <a href='/' className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/tokkaya79/Shop-Project" target="_blank" rel="noreferrer">Repo</a></li>
                     <li><a href="badges.html">Components</a></li>
                     <li><a href="collapsible.html">JavaScript</a></li>
                 </ul>
